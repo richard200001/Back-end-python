50e2e56e865bb94ba2e5f0a78a2af1239b7ae1f2
richard200001 <vegar7639@gmail.com>
2021-10-13 11:58:01 -0500

    Actualizaciónvistasyurls

---
+++ authApp/views/consultaView.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from rest_framework import status, views
+from rest_framework import response
+from rest_framework.response import Response
+
+from authApp.models.consulta import Consulta
+from authApp.serializers.consultaSerializer import ConsultaSerializer
+
+class consultaView(views.APIView):
+
+    def get(self, request, *args, **kwargs):
+        consult=Consulta.objects.all()
+        serializer=ConsultaSerializer(consult,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
